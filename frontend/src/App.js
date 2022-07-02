da7e6fc793f8f23b862c576871ce281076501dd4
Amit <amitsingh90748@gmail.com>
2022-07-03 05:24:25 +0530

    initial-commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header>
+       <a href="/" >amazona</a>
+      </header>
+      <body>
+        <p>Let Begin</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
