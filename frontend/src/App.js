cdcba5b7869c3de2a0948524cd5afe128804c5a1
Amit <amitsingh90748@gmail.com>
2022-07-03 06:54:51 +0530

    list-items added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 
+import data from './data';
 import './App.css';
 
 function App() {
@@ -7,9 +8,27 @@ function App() {
       <header>
        <a href="/" >amazona</a>
       </header>
-      <body>
-        <p>Let Begin</p>
-      </body>
+      <main>
+        <h1>Featured Products</h1>
+       <div className = "products">
+        {
+          data.products.map(product => (
+          <div className ="product"key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+              <img src={product.image} alt ={product.name} />
+            </a>  
+
+            <div className="product-info">
+              <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Add to Cart</button>
+            </div>
+          </div>))
+        }
+        </div>
+      </main>
     </div>
   );
 }
