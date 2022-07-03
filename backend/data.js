c186c99c33eb36fe40432741fa5273afcb9ed99a
Amit <amitsingh90748@gmail.com>
2022-07-03 16:20:54 +0530

    backend added

---
+++ backend/data.js
@@ -0,0 +1,61 @@
+const data ={
+
+    products: [
+         {
+            name : 'Nike Slim Shirt',
+            slug : 'nike_Slim_shit',
+            category: 'Shirts',
+            image : '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews : 10,
+            discription: 'high quality products'
+
+         } ,
+         {
+            name : 'Adida Slim Shirt',
+            slug : 'adidas_Slim_shit',
+            category: 'Shirts',
+            image : '/images/p2.jpg',
+            price: 250,
+            countInStock: 27,
+            brand : 'Adidas',
+            rating : 4.7,
+            numReviews : 53,
+            discription: 'high quality products'
+
+         },
+         {
+            name : 'Puma Slim Shirt',
+            slug : 'puma_Slim_shit',
+            category: 'Shirts',
+            image : '/images/p3.jpg',
+            price: 120,
+            countInStock: 10,
+            brand : 'Puma',
+            rating : 4.5,
+            numReviews : 10,
+            discription: 'high quality products'
+
+         } ,
+         {
+            name : 'Cobs Slim Shirt',
+            slug : 'cobs_Slim_shit',
+            category: 'Shirts',
+            image : '/images/p4.jpg',
+            price: 250,
+            countInStock: 27,
+            brand : 'Cobs',
+            rating : 4.7,
+            numReviews : 53,
+            discription: 'high quality products'
+
+         }
+
+
+    ]
+};
+
+export default data;
